5b433baa3e0527b3ece9ca4fbe58cb502786e3d5
Hsruka <estherlstplw2003@gmail.com>
2026-02-04 14:44:19 +0700

    Add SSL to db config

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -5,7 +5,7 @@ const dbConfig = {
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  ssl: { rejectUnauthorized: true }
+  ssl: { rejectUnauthorized: false}
 };
 
 // ดึงข้อมูลขยะทั้งหมดจากทุก user
--- controllers/wasteController.js
+++ controllers/wasteController.js
@@ -11,7 +11,7 @@ const dbConfig = {
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  ssl: { rejectUnauthorized: true }
+  ssl: { rejectUnauthorized: false}
 };
 
 // --- Model and Metadata variables ---
